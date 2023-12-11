3ceb760d2750147d9a1ab5d5d09a1ae170e63359
denis <denniiskim@gmail.com>
2023-12-11 13:23:45 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,25 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+		<div>
+			<NavBar />
+			<Switch>
+				<Route path="/movies">
+					<Movies />
+				</Route>
+				<Route path="/directors">
+					<Directors />
+				</Route>
+				<Route path="/actors">
+					<Actors />
+				</Route>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+	const showDirectors = directors.map((director) => (
+		<div key={director.name}>
+			{director.name}
+			<ul>
+				{director.movies.map((movie) => (
+					<li key={movie}>{movie}</li>
+				))}
+			</ul>
+		</div>
+	));
+
+	return (
+		<div>
+			<h1>Directors Page</h1>
+			{showDirectors}
+		</div>
+	);
 }
 
 export default Directors;
